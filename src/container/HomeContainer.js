6da590e3283cfc354873e5d4a07fd2ab43bc4d69
Aman-Raut <amanraut13@gmail.com>
2024-02-05 02:04:06 +0530

    modified:   src/container/HomeContainer.js 	modified:   src/index.js 	modified:   src/services/Constants.js 	modified:   src/services/Reducer/Index.js 	new file:   src/services/Reducer/Reducer.js 	modified:   src/services/action/Actions.js

--- src/container/HomeContainer.js
+++ src/container/HomeContainer.js
@@ -1,4 +1,13 @@
+import {connect} from 'react-redux';
+import {addToCart} from '../services/action/Actions'
 import Home from '../components/Home'
 
+const mapStateToProps=state=>({
 
-export default Home;
+})
+const mapDispatchToProps=dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)()
+//export default Home;
